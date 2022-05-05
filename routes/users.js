d50bbe3c95c165fc5dc72f0d41b79d0c871bc3a8
bahram <bahram101@mail.ru>
2022-05-05 18:59:48 +0600

    finish follow route

--- routes/users.js
+++ routes/users.js
@@ -55,11 +55,20 @@ router.put('/:id/follow', async (req, res) => {
 		try {
 			const user = await User.findById(req.params.id);
 			const currentUser = await User.findById(req.body.userId);
+			if (!user.followers.includes(req.body.userId)) {
+				await user.updateOne({ $push: { followers: req.body.userId } });
+				await currentUser.updateOne({
+					$push: { followings: req.params.id },
+				});
+				res.status(200).json('User has been followed');
+			} else {
+				res.status(403).json('You already follow this user');
+			}
 		} catch (err) {
-			res.status(500).json(err)
+			res.status(500).json(err);
 		}
 	} else {
-		res.status(403).json("You can't follow yourself")
+		res.status(403).json("You can't follow yourself");
 	}
 });
 

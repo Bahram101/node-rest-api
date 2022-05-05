632b2481eab147f5b48091f022ee11ad283112d0
bahram <bahram101@mail.ru>
2022-05-05 18:41:52 +0600

    add follow route

--- routes/users.js
+++ routes/users.js
@@ -44,9 +44,23 @@ router.delete('/:id', async (req, res) => {
 router.get('/:id', async (req, res) => {
 	try {
 		const user = await User.findById(req.params.id);
-		const {password, updatedAt, ...other} = user._doc; 
-		res.status(200).json(other); 
+		const { password, updatedAt, ...other } = user._doc;
+		res.status(200).json(other);
 	} catch (error) {}
+});
+
+//follow a user
+router.put('/:id/follow', async (req, res) => {
+	if (req.body.userId !== req.params.id) {
+		try {
+			const user = await User.findById(req.params.id);
+			const currentUser = await User.findById(req.body.userId);
+		} catch (err) {
+			res.status(500).json(err)
+		}
+	} else {
+		res.status(403).json("You can't follow yourself")
+	}
 });
 
 module.exports = router;
